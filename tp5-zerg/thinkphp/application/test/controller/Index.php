1aa21fa8d010084cf4dfb469c028a3c5d1b29361
eson <834767372@qq.com>
2019-04-18 14:29:56 +0800

    【更新】- 测试数据库连接的test控制器

---
+++ tp5-zerg/thinkphp/application/test/controller/Index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: eson
+ * Date: 2019-04-18
+ * Time: 14:28
+ */
+
+namespace app\test\controller;
+
+
+use think\Controller;
+
+class Index extends Controller
+{
+    public function index ()
+    {
+        /*测试数据库*/
+        $ret = \think\Db::name('test')->find();
+        dump($ret);
+    }
+}
